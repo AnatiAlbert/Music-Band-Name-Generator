3f23d212bececbe6db957a5384e016ad3959ca51
Albert Anati <anatiprogrammer@gmail.com>
2023-02-21 16:09:58 +0000

    Music Band Name Generator

---
+++ BandNameGen.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print('Hello, nice to visit band generator program!')
+#2. Ask the user for the city that they grew up in.
+city = input('Where did you grow up in?\n')
+#3. Ask the user for the name of a pet.
+pet = input('What is your pet called?\n')
+#4. Combine the name of their city and pet and show them their band name.
+print("Band name is " + city + " " + pet)
